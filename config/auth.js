9bf128bbac9c0f2555e9b265c8fa632a05bb2fba
clouie87 <carol.louie@gmail.com>
2015-02-17 23:31:02 -0400

    adding instagram  to clouie

--- config/auth.js
+++ config/auth.js
@@ -5,13 +5,21 @@ module.exports = {
         'clientID'      : '813365472032554', // your App ID
         'clientSecret'  : 'f32ed4d4b39008a290686ae744f88c8f', // your App Secret
         'callbackURL'   : 'http://clouie.ca/auth/facebook/callback'
-        //},
+        },
+
+      'instagramAuth': {
+          clientID        : '76f69ff2e0564fce894b6ab73de814a1',
+          clientSecret    : '45b2298dd368416b996a1f19625c7eb9',
+          callbackURL     : 'http://clouie.ca/auth/instagram/callback'
+
+
 
 //    'twitterAuth' : {
 //        'consumerKey'       : 'your-consumer-key-here',
 //        'consumerSecret'    : 'your-client-secret-here',
 //        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
 //    },
+
 //
 //    'googleAuth' : {
 //        'clientID'      : 'your-secret-clientID-here',

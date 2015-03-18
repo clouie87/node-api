fabc5778481b7cc6dc236c57b1fe2228ba2eb8a2
clouie87 <carol.louie@gmail.com>
2015-03-18 10:54:53 -0300

    trying to get passport to function with mobile

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(expressValidator());
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
 

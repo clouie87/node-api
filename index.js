8cdf48a508da8e2854d5a22846cc2c86294cfc0f
clouie87 <carol.louie@gmail.com>
2015-03-18 18:41:49 -0300

    debugging because im silly

--- index.js
+++ index.js
@@ -279,7 +279,7 @@ challengeRouter.post('/', multer({
         req.body.name,
         req.files.photo.path,
         req.body.description,
-        req.body.user_id
+        req.user.id
     ];
     //multer appends the field name (photo)
 

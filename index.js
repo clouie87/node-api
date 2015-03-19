65abd948353edb6ad08985a00bc833ac3d028e3b
clouie87 <carol.louie@gmail.com>
2015-03-19 18:13:29 -0300

    accessing my accepted table

--- index.js
+++ index.js
@@ -352,7 +352,7 @@ acceptRouter.get('/', function(req, res){
   res.header("Access-Control-Allow-Origin", "http://localhost:8100");//set cross domain so localhost:8100 can access clouie.ca
   res.header("Access-Control-Allow-Headers", "X-Requested-With");//make it so allow headers with x request. Without it we get similar error: "XMLHttpRequest cannot load http://...
 
-    var sql = 'SELECT accepted_challenge.*, users.name as username FROM accepted_challenge, users WHERE accepted_challenge.u_id = user.u_id OFFSET $1 LIMIT $2';
+    var sql = 'SELECT accepted_challenge.*, user.name as username FROM accepted_challenge, users WHERE accepted_challenge.u_id = user.u_id OFFSET $1 LIMIT $2';
     postgres.client.query(sql, function (err, result) {
       if (err) {
         console.error(err);

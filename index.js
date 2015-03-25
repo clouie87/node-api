2682c6275fef4be0f3711367948565bacb0c4fea
clouie87 <carol.louie@gmail.com>
2015-03-25 12:33:14 -0300

    trying to create a vote.lookup function to search through number of votes

--- index.js
+++ index.js
@@ -514,7 +514,7 @@ voteRouter.post('/', function(req, res) {
   });
 });
 voteRouter.get('/:id([0-9]+)', votes.lookupPhoto, function(req, res) {
-  console.log('getting votes/id');
+  console.log('getting vote/id');
   res.header("Access-Control-Allow-Origin", "http://localhost:8100");//set cross domain so localhost:8100 can access clouie.ca
   res.header("Access-Control-Allow-Headers", "X-Requested-With");//make it so allow headers with x request. Without it we get similar error: "XMLHttpRequest cannot load http://...
   res.json(req.vote);
